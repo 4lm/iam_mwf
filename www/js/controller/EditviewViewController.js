0a1c117a9110c65367041ad056b6b7a59e5699f5
Alexis Michaltsis <alexis@michaltsis.net>
2019-06-22 20:34:30 +0200

    Clean code in EditviewViewController

--- www/js/controller/EditviewViewController.js
+++ www/js/controller/EditviewViewController.js
@@ -22,21 +22,7 @@ export default class EditviewViewController extends mwf.ViewController {
      */
     async oncreate() {
 
-        // Ternary check of edit and create cases
         this.mediaItem = (this.args && this.args.item) ? this.args.item : new entities.MediaItem();
-
-        // Alternativ, to above ternary operator:
-        //
-        // // Edit case
-        // if (this.args && this.args.item) {
-        //     this.mediaItem = this.args.item;
-        // }
-        // // Create case
-        // else {
-        //     this.mediaItem = new entities.MediaItem();
-        // }
-
-        // this.bindElement("mediaEditviewTemplate", { item: this.mediaItem }, this.root);
 
         this.viewProxy = this.bindElement(
             "mediaEditviewTemplate",
@@ -53,9 +39,6 @@ export default class EditviewViewController extends mwf.ViewController {
                     }),
                     deleteItem: ((event) => {
                         this.hideDialog();
-                        // this.mediaItem.delete().then(() => {
-                        //     this.nextView("mediaOverview", this.args);
-                        // });
                         this.mediaItem.delete().then(() => {
                             this.notifyListeners(new mwf.Event("crud", "deleted", "MediaItem", this.mediaItem._id));
                             this.previousView();
@@ -115,8 +98,6 @@ export default class EditviewViewController extends mwf.ViewController {
             return false;
         };
 
-
-
         // call the superclass once creation is done
         super.oncreate();
     }
@@ -131,7 +112,6 @@ export default class EditviewViewController extends mwf.ViewController {
     }
 
     createOrEditMediaItem() {
-        // alert("submit! mediaItem: " + JSON.stringify(this.mediaItem));
 
         // If mediaItem already exists, then update.
         if (this.mediaItem.created) {
@@ -146,30 +126,7 @@ export default class EditviewViewController extends mwf.ViewController {
                 this.previousView();
             });
         }
-    }
 
-    /*
-     * for views with listviews: bind a list item to an item view
-     * TODO: delete if no listview is used or if databinding uses ractive templates
-     */
-    bindListItemView(viewid, itemview, item) {
-        // TODO: implement how attributes of item shall be displayed in itemview
-    }
-
-    /*
-     * for views with listviews: react to the selection of a listitem
-     * TODO: delete if no listview is used or if item selection is specified by targetview/targetaction
-     */
-    onListItemSelected(listitem, listview) {
-        // TODO: implement how selection of listitem shall be handled
-    }
-
-    /*
-     * for views with listviews: react to the selection of a listitem menu option
-     * TODO: delete if no listview is used or if item selection is specified by targetview/targetaction
-     */
-    onListItemMenuItemSelected(option, listitem, listview) {
-        // TODO: implement how selection of option for listitem shall be handled
     }
 
     /*
@@ -183,12 +140,4 @@ export default class EditviewViewController extends mwf.ViewController {
         // TODO: implement action bindings for dialog, accessing dialog.root
     }
 
-    /*
-     * for views that initiate transitions to other views
-     */
-    async onReturnFromSubview(subviewid, returnValue, returnStatus) {
-        // TODO: check from which view, and possibly with which status, we are returning, and handle returnValue accordingly
-    }
-
 }
-
