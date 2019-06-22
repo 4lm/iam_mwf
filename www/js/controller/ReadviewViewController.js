cefee04ccac832376cb5e81715ffc8423ed27187
Alexis Michaltsis <alexis@michaltsis.net>
2019-06-22 20:27:21 +0200

    Clean code in ReadviewViewController

--- www/js/controller/ReadviewViewController.js
+++ www/js/controller/ReadviewViewController.js
@@ -62,30 +62,6 @@ export default class ReadviewViewController extends mwf.ViewController {
     }
 
     /*
-     * for views with listviews: bind a list item to an item view
-     * TODO: delete if no listview is used or if databinding uses ractive templates
-     */
-    bindListItemView(viewid, itemview, item) {
-        // TODO: implement how attributes of item shall be displayed in itemview
-    }
-
-    /*
-     * for views with listviews: react to the selection of a listitem
-     * TODO: delete if no listview is used or if item selection is specified by targetview/targetaction
-     */
-    onListItemSelected(listitem, listview) {
-        // TODO: implement how selection of listitem shall be handled
-    }
-
-    /*
-     * for views with listviews: react to the selection of a listitem menu option
-     * TODO: delete if no listview is used or if item selection is specified by targetview/targetaction
-     */
-    onListItemMenuItemSelected(option, listitem, listview) {
-        // TODO: implement how selection of option for listitem shall be handled
-    }
-
-    /*
      * for views with dialogs
      * TODO: delete if no dialogs are used or if generic controller for dialogs is employed
      */
@@ -101,7 +77,6 @@ export default class ReadviewViewController extends mwf.ViewController {
      */
     async onReturnFromSubview(subviewid, returnValue, returnStatus) {
         // TODO: check from which view, and possibly with which status, we are returning, and handle returnValue accordingly
-        // console.log("onReturnFromSubview Readview", subviewid, returnValue, returnStatus);
         if (subviewid == "mediaEditview") {
             if (returnStatus == "updated" && returnValue) {
                 this.viewProxy.update(returnValue);
